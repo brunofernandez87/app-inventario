bef1e792966f43adbcf7b93ee835eb1008ba3e29
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-08-18 17:29:22 -0300

    cambio de layout y de producto

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { ListaProductoProvider } from "@/context/listaProductoContext";
 import { Stack } from "expo-router";
 import { AuthProvider } from "../context/authContext";
 import { EmpresaProvider } from "../context/empresaContext";
@@ -6,8 +7,10 @@ export default function LayoutPrincipal() {
   return (
     <AuthProvider>
       <EmpresaProvider>
-        {/* El Stack maneja la navegación visual ocultando la barra superior nativa */}
-        <Stack screenOptions={{ headerShown: false }} />
+        <ListaProductoProvider>
+          {/* El Stack maneja la navegación visual ocultando la barra superior nativa */}
+          <Stack screenOptions={{ headerShown: false }} />
+        </ListaProductoProvider>
       </EmpresaProvider>
     </AuthProvider>
   );
