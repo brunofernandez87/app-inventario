e60c1aefa996c283acbade73785a4ab5d0c99b99
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-08-18 19:35:28 -0300

    cambio para que todo funcione

---
+++ src/app/movimiento_stock/movimiento_stock.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function PantallaDetalleVenta() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Pantalla de movimiento stock (En construcción)</Text>
+    </View>
+  );
+}
---
+++ src/app/reporte_,mensual/reporte_mensual.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function PantallaDetalleVenta() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Pantalla de reporte mensual (En construcción)</Text>
+    </View>
+  );
+}
