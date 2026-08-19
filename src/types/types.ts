7d92b70741e333814aae470fbd63c95335948a6d
brunofernandez87 <brunoemanuelfernandez1@gmail.com>
2026-08-19 18:47:09 -0300

    modificacion de type de producto

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,20 +1,21 @@
 export interface Producto {
   id_producto: number;
   id_empresa: number;
+  codigo_alfanumerico: string;
   codigo_barras?: string;
   nombre_producto: string;
   marca?: string;
   ubicacion?: string;
-  costo_compra?: number;
-  precio_venta?: number;
-  id_tipo_venta?: number;
-  stock_unidades: number;
-  stock_paquetes: number;
-  unidades_por_paquete: number;
-  bonificacion_paquete: number;
-  stock_minimo: number;
-  alerta_proyeccion: boolean;
-  fecha_creacion: string;
+  costo_compra: number;
+  precio_venta: number;
+  id_medida: number;
+  stock_unidades?: number;
+  stock_paquetes?: number;
+  unidades_por_paquete?: number;
+  bonificacion_paquete?: number;
+  stock_minimo?: number;
+  alerta_proyeccion?: boolean;
+  fecha_creacion?: string;
 }
 
 export interface Empresa {
